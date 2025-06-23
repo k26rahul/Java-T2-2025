728c9fbdc41d41524e26b7f036ac8332bde6bc1a
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-06-23 20:12:48 +0530

    .gitignore

---
+++ Hello.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+
+public class Hello {
+  public static void main(String[] args) {
+    // byte a = 100;
+    // int a_ = (int) a;
+    // a_ = 600;
+
+    // int a = 5;
+    // int b = 2;
+    // System.out.println(a / (double) b);
+
+    // String message = "hello";
+    // char[] messageLetters = message.toCharArray();
+    // for (char c : messageLetters) {
+    // System.out.println(c);
+    // }
+
+    // for (int i = 0; i < messageLetters.length; i++) {
+    // System.out.printf("i=%d, letter=%s \n", i, messageLetters[i]);
+    // }
+
+    // System.out.println(message.toCharArray());
+
+    // char[] charArray = {'a', 'p'};
+    // for (int i = 0; i < charArray.length; i++) {
+    // System.out.println(charArray[i]);
+    // }
+
+    // System.out.println(message.charAt(message.length() - 2));
+
+    // for (int i = 0; i < message.length(); i++) {
+    // System.out.println(message.charAt(i));
+    // }
+
+    // for (char c : message.toCharArray()) {
+    // System.out.println(c);
+    // }
+
+    // char[] lettersOfMessage = message.toCharArray();
+    // for (char c : lettersOfMessage) {
+    // System.out.println(c);
+    // }
+
+    // for (int i = 0; i < lettersOfMessage.length; i++) {
+    // if (i % 2 == 0) {
+    // System.out.println(lettersOfMessage[i]);
+    // }
+    // }
+
+    // for (int i = 0; i < lettersOfMessage.length; i++) {
+    // System.out.println(lettersOfMessage[i]);
+    // }
+
+    // for (char c : lettersOfMessage) {
+    // System.out.println(c);
+    // }
+
+    // char[] messageLetters = message.toCharArray();
+    // for (char c : messageLetters) {
+    // System.out.println(c);
+    // }
+
+    // for (char c : message.toCharArray()) {
+    // System.out.println(c);
+    // }
+  }
+}
