6129571088a7a7d8dc95ca1163a2b74617013ac8
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-07-27 17:42:25 +0530

    Quiz 2 prep

---
+++ Boat.java
@@ -0,0 +1,13 @@
+public class Boat {
+  public String name;
+
+  public Boat(String name) {
+    this.name = name;
+  }
+
+  public static void main(String[] args) {
+    Boat viduBoat = new Boat("vidu's boat");
+    Boat paruBoat = new Boat("paru's boat");
+    System.out.println("hello");
+  }
+}
